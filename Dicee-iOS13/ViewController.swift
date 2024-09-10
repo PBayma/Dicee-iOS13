58ac69a57db1018e0ae832db5be3188659bf4abd
Pedro Bayma <pedrobayma01@gmail.com>
2024-09-09 21:55:09 -0300

    feat: creating functionality to button and randomizing dices

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,37 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutLet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = [
+        "DiceOne",
+        "DiceTwo",
+        "DiceThree",
+        "DiceFour",
+        "DiceFive",
+        "DiceSix",
+    ]
+    
+    // IBAction allows me to reference a click in a button
+    @IBAction func rollButtonAction(_ sender: Any) {
+        let leftDice = diceArray.randomElement() ?? "DiceOne"
+        let rightDice = diceArray.randomElement() ?? "DiceOne"
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: leftDice)
+        diceImageView2.image = UIImage(imageLiteralResourceName: rightDice)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Change image value to another image in assets
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
     }
 
-
 }
-
